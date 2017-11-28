6433344affaa741740261beb524c189aa82e22e1
Jennifer Dufresne <31451903+jendufresne@users.noreply.github.com>
2017-11-28 10:43:40 -0500

    Add files via upload
    updated code to fit PEP8 requirements

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,23 +2,33 @@ import fresh_tomatoes
 import media
 
 lion_king = media.Movie("Lion King",
-                        "The story of a lion cub's journey to adulthood and acceptance of his royal destiny.",
-                        "https://upload.wikimedia.org/wikipedia/en/thumb/3/3d/The_Lion_King_poster.jpg/220px-The_Lion_King_poster.jpg",
+                        "The story of a lion cub's journey to adulthood"
+                        " and acceptance of his royal destiny.",
+                        "https://upload.wikimedia.org/wikipedia/en/thumb/3/3d/The_Lion_King_poster.jpg/220px-The_Lion_King_poster.jpg",  # NOQA
                         "https://www.youtube.com/watch?v=V6jOvVf05aQ")
 five_hundred_days_of_summer = media.Movie("500 Days of Summer",
-                                 "An offbeat romantic comedy about a woman who doesn't believe true love exists, and the young man who falls for her.",
-                                 "https://upload.wikimedia.org/wikipedia/en/d/d1/Five_hundred_days_of_summer.jpg",
+                                         "An offbeat romantic comedy about a "
+                                         "woman who doesn't believe true love"
+                                         " exists, and the young man who "
+                                         "falls for her.",
+                                 "https://upload.wikimedia.org/wikipedia/en/d/d1/Five_hundred_days_of_summer.jpg",  # NOQA
                                  "https://www.youtube.com/watch?v=PsD0NpFSADM")
-                                 
+
 ferris_bueller = media.Movie("Ferris Bueller's Day Off",
-                             "A high school wise guy is determined to have a day off from school, despite what the Principal thinks of that.",
-                             "https://upload.wikimedia.org/wikipedia/en/9/9b/Ferris_Bueller%27s_Day_Off.jpg",
+                             "A high school wise guy is determined "
+                             "to have a day off from school, despite what "
+                             "the Principal thinks of that.",
+                             "https://upload.wikimedia.org/wikipedia/en/9/9b/Ferris_Bueller%27s_Day_Off.jpg",  # NOQA
                              "https://www.youtube.com/watch?v=D6gABQFR94U")
 
 spirited_away = media.Movie("Spirited Away",
-                            "During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts.",
-                            "https://images-na.ssl-images-amazon.com/images/M/MV5BOGJjNzZmMmUtMjljNC00ZjU5LWJiODQtZmEzZTU0MjBlNzgxL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
+                            "During her family's move to the suburbs, "
+                            "a sullen 10-year-old girl wanders into a world "
+                            "ruled by gods, witches, and spirits, "
+                            "and where humans are changed into beasts.",
+                            "https://images-na.ssl-images-amazon.com/images/M/MV5BOGJjNzZmMmUtMjljNC00ZjU5LWJiODQtZmEzZTU0MjBlNzgxL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",  # NOQA
                             "https://www.youtube.com/watch?v=ByXuk9QqQkk")
 
-movies = [lion_king, five_hundred_days_of_summer, ferris_bueller, spirited_away]
+movies = [lion_king, five_hundred_days_of_summer, ferris_bueller, spirited_away]  # NOQA
+
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,6 +1,8 @@
 import webbrowser
+
+
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, title, storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
